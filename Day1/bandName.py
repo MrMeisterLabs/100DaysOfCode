214cca26664bf4272979432a7a4281ef4539c3dc
MrMeisterLabs <sidtrade@mail.com>
2022-09-09 08:08:57 +0300

    Day 1 - [NF] Creating a bandName generator
    Pushing the first bandname file to the git repo.

---
+++ Day1/bandName.py
@@ -0,0 +1,15 @@
+# Welcome the user
+
+print("Welcome to the Band Name generator. 2 quick questions!")
+
+# Ask the user about where they were born
+
+city = input("Which city were you born in?: \n ")
+
+# Ask the user about their first pet's name
+
+pet = input("What was the name of your first pet?: \n ")
+
+# Combine the city and pet name to suggest a band name
+
+print(f"Cool! Your new bandname is {city} {pet}!")
