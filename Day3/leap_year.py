2439e66c8987c97547f8a23bc561b4074d979404
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-12 08:42:33 +0300

    Adding leap year calculation program code

---
+++ Day3/leap_year.py
@@ -0,0 +1,18 @@
+# Ask user for a year and check if it is a leap year
+
+year = int(input("Please type in a year: "))
+
+# If a year is divisible by 4, it is a leap year
+if year % 4 == 0:
+    # If a year is ALSO divisible by 100 and 400, it is a leap year
+    if year % 100 == 0: 
+        # If the year % 100 is TRUE, then run the following IF statement
+        if year % 400 == 0:
+            print(f"{year} is a leap year.")
+        # If a year is NOT divisible by 400, it is not a leap year
+        if year % 400 != 0:
+            print(f"{year} is not a leap year.")
+    else:
+        print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
