df0604bef7884a9959780bec6055ea68a56f6698
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-14 14:52:40 +0300

    Complete Treasure Island

--- Day3/treasure_island.py
+++ Day3/treasure_island.py
@@ -20,3 +20,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
+# Welcome message
+print("Welcome to Tresure Island!")
+print("Your mission is to find the treasure.")
+direction = input(
+    'You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if direction == "left":
+    choice = input(
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice == "wait":
+        choice3 = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
