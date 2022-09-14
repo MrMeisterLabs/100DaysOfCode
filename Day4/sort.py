932f1b26525deb4c01e20fda38a853b7e9669172
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-14 14:53:46 +0300

    Day4: [NFD] Trying to work on the sort algorithm.

---
+++ Day4/sort.py
@@ -0,0 +1,29 @@
+# x, y = input("Enter two values separated by one space: ").split()
+# print("Character 1: ", x)
+# print("Character 2: ", y)
+# print()
+
+myList = ["a","n","y"]
+
+# while True:
+#   alphabet = input("Enter a character (Type 'q' to quit): ")
+#   if alphabet == 'q':
+#     break
+#   myList.append(alphabet)
+
+if myList[0] > myList[1]:
+    print("a" > "b")
+elif myList[0] > myList[2]:
+    print("b" > "a")
+
+# mylist = [1, 2, 3]
+# mylist[0], mylist[1] = mylist[1], mylist[0]
+# print(mylist) # [2, 1, 3]
+
+# Take 5 inputs from a user and store them in a list.
+
+# myList = []
+# for i in range(5):
+#    myList.append(input("Enter a character: "))
+#    print(myList)
+
