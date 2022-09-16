ba7ccecd3bce6de417dd79b919c77bf48472a784
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-16 08:55:02 +0300

    Day4[NFD]: Treasure Map first draft. ToDo: Get input from the user and place the treasure in the map

---
+++ Day4/TreasureMap.py
@@ -0,0 +1,12 @@
+Create three 
+
+row1 = ["⬜️", "⬜️", "⬜️"]
+row2 = ["⬜️", "⬜️", "⬜️"]
+row3 = ["⬜️", "⬜️", "⬜️"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = print("Specify and column number and row number where you'd like to place the treasure (e.g. 13): ")
+selected_column = map
+
