ef03c4f7f73abfcb5b5548347ea55c3b96250f1e
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-16 08:41:45 +0300

    Day4[NF]: Nested lists example, that helps us understand how maps work by combining lists

---
+++ Day4/Nested_Lists.py
@@ -0,0 +1,11 @@
+    
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+
+# In the line below, the first index [1] refers to the vegetables list, and the second index [1] refers to the second item in the vegetables list.
+
+print(dirty_dozen[1][1])
+
+# In the line below, the first index [0] refers to the fruits list, and the second index [1] refers to the second item in the fruits list.
+print(dirty_dozen[0][1])
