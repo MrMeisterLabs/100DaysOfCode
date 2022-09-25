59441d906e4da9c6446c3bf124d1155b9d9feb48
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-09-25 21:10:35 +0300

    Day7[UF]: Hangman final version.

--- Day7/Hangman-Final.py
+++ Day7/Hangman-Final.py
@@ -1,108 +1,51 @@
-#Step 4
-
+from hangman_art import stages, logo, welcome
+from hangman_words import word_list
 import random
+from replit import clear
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-print('''
- /$$      /$$           /$$                                            
-| $$  /$ | $$          | $$                                            
-| $$ /$$$| $$  /$$$$$$ | $$  /$$$$$$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ 
-| $$/$$ $$ $$ /$$__  $$| $$ /$$_____/ /$$__  $$| $$_  $$_  $$ /$$__  $$
-| $$$$_  $$$$| $$$$$$$$| $$| $$      | $$  \ $$| $$ \ $$ \ $$| $$$$$$$$
-| $$$/ \  $$$| $$_____/| $$| $$      | $$  | $$| $$ | $$ | $$| $$_____/
-| $$/   \  $$|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$ | $$ | $$|  $$$$$$$
-|__/     \__/ \_______/|__/ \_______/ \______/ |__/ |__/ |__/ \_______/
-''')
+# Print welcome
+print(logo)
+# Print hangman
+print(welcome)
 
 end_of_game = False
-word_list = ["aardvark", "camel", "chewbaca", "dinosaur", "elephant", "falcon", "giraffe", "hippopotamus", "iguana", "jaguar", "kangaroo", "lemur", "manatee", "narwhal", "octopus", "panda", "quail", "rhinoceros", "salamander", "tiger", "unicorn", "vulture", "wombat", "xenops", "yak", "zebra"]
-chosen_word = random.choice(word_list)
+words_list = word_list
+chosen_word = random.choice(words_list)
 word_length = len(chosen_word)
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
+print(f'\nPssst, the solution is {chosen_word}.')
+
+# Create a variable called 'lives' to keep track of the number of lives left. 
+# Set 'lives' to equal 6.
 
 lives = 6
 
-#Testing code
-#print(f'Pssst, the solution is {chosen_word}.')
+# Testing code
 
-#Create blanks
+# Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
 
 while not end_of_game:
+    guess_list = []
     guess = input("Guess a letter: ").lower()
+    guess_list.append(guess)
+    print(guess_list)
+    clear()
+
+    if guess not in guess_list:
+      continue
 
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+
         if letter == guess:
             display[position] = letter
             print(f"Good guess! {lives} attempts left, still. Keep going.")
 
 
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
         lives -= 1
         print(f"\nTry again! {lives} guesses remaining.\n")
@@ -111,17 +54,14 @@ while not end_of_game:
             end_of_game = True
             print("You lose.")
             break
-        # else:
-        #     print(stages[lives])
-        #     break
 
-    #Join all the elements in the list and turn it into a String.
+    # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}\n")
 
-    #Check if user has got all letters.
+    # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    # Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(stages[lives])
