689acf04740a7903bc58e6efbb156a3ecdab2fe4
Sid Prasad <Sid.Prasad@relexsolutions.com>
2022-10-06 22:15:44 +0300

    Day10[UF]: PEP8 based formatting improvements

--- Day10/calculator-start.py
+++ Day10/calculator-start.py
@@ -4,13 +4,16 @@
 def add(n1, n2):
     return n1 + n2
 
+
 # Subtract
 def subtract(n1, n2):
     return n1 - n2
 
+
 # Multiply
 def multiply(n1, n2):
     return n1 * n2
+
 
 # Divide
 def divide(n1, n2):
@@ -19,14 +22,16 @@ def divide(n1, n2):
 
 # Dictionary to store operation types
 operations = {
-    "+" : add, 
-    "-" : subtract, 
-    "*" : multiply, 
-    "/" : divide
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
 }
 
 # Ask the user for the first number
 num1 = int(input("Type in the first number:  "))
+
+# Create a while loop that asks the user for next operation and the next number
 
 # Loop thorugh the dictionary to display the list of symbols for the user
 for operation in operations:
@@ -38,14 +43,13 @@ operation_symbol = input("Choose an operation type from the symbols above: ")
 # Ask the user for the second number
 num2 = int(input("\nType in the second number: "))
 
-# Create a variable that picks a value from the operations dictionary based on the chosen user symbol
+# Create a variable that picks a value from the operations dictionary
 calculation_function = operations[operation_symbol]
 answer = calculation_function(num1, num2)
 
 # Loop through the operations dictionary's keys (+, -, *, /)
 for operation in operations:
-    
-    # If operation is equal to the operation_symbol chosen by the user print "answer"
+
+    # If operation is equal to the operation_symbol, print "answer"
     if operation == operation_symbol:
         print(f"\n{num1} {operation_symbol} {num2} = {answer}")
-
